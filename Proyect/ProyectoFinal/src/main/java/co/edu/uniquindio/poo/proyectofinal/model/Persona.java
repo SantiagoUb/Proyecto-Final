7b085e5edc27016c74dec244063dec5d6f7060bf
SORGIE-m <159481227+SORGIE-m@users.noreply.github.com>
2025-05-10 10:09:22 -0500

    commit Persona

---
+++ Proyect/ProyectoFinal/src/main/java/co/edu/uniquindio/poo/proyectofinal/model/Persona.java
@@ -0,0 +1,47 @@
+package co.edu.uniquindio.poo.proyectofinal.model;
+
+public class Persona {
+    protected String nombre;
+    protected String id;
+    protected String telefono;
+    protected String email;
+
+    public Persona(String nombre, String id, String telefono, String email) {
+        this.nombre = nombre;
+        this.id = id;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
