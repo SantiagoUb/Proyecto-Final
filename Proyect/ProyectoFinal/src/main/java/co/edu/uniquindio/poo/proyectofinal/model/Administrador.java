4cc10d899f992dda312a8911c0b4c3088b1a6428
SORGIE-m <159481227+SORGIE-m@users.noreply.github.com>
2025-05-10 11:06:25 -0500

    commit crear administrador

---
+++ Proyect/ProyectoFinal/src/main/java/co/edu/uniquindio/poo/proyectofinal/model/Administrador.java
@@ -0,0 +1,23 @@
+package co.edu.uniquindio.poo.proyectofinal.model;
+
+public class Administrador extends Persona{
+    public Administrador(String nombre, String id, String telefono, String email){
+        super(nombre, id, telefono, email);
+    }
+
+    public void gestionarUsuarios(){
+
+    }
+
+    public void asignarMedico(){
+
+    }
+
+    public void gestionarSala(){
+
+    }
+
+    public void generarInforme(){
+
+    }
+}
