26755003f8cb170a0833f63a14293d39a16a3909
SORGIE-m <159481227+SORGIE-m@users.noreply.github.com>
2025-05-10 10:47:59 -0500

    commit medico sin metodos

---
+++ Proyect/ProyectoFinal/src/main/java/co/edu/uniquindio/poo/proyectofinal/model/Medico.java
@@ -0,0 +1,25 @@
+package co.edu.uniquindio.poo.proyectofinal.model;
+
+public class Medico extends Persona{
+
+    public Medico(String nombre, String id, String telefono, String email){
+        super(nombre, id, telefono, email);
+    }
+
+    public void registrarDiagnostico (){
+
+    }
+
+    public void administrarHorarios(){
+
+    }
+
+    public void consultarHistorial () {
+
+    }
+
+    public void recetarMedicamento(){
+
+    }
+    
+}
