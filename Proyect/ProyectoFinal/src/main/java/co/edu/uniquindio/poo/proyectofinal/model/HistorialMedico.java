e96a19de2686dad10c9340d4108d1c32446549ca
SORGIE-m <159481227+SORGIE-m@users.noreply.github.com>
2025-05-10 12:33:39 -0500

    commit HistorialMedico

---
+++ Proyect/ProyectoFinal/src/main/java/co/edu/uniquindio/poo/proyectofinal/model/HistorialMedico.java
@@ -0,0 +1,48 @@
+package co.edu.uniquindio.poo.proyectofinal.model;
+
+public class HistorialMedico {
+    private String idHistorial;
+    private String notasAdicionales;
+
+    public HistorialMedico(String idHistorial, String notasAdicionales) {
+        this.idHistorial = idHistorial;
+        this.notasAdicionales = notasAdicionales;
+    }
+
+    public String getIdHistorial() {
+        return idHistorial;
+    }
+
+    public void setIdHistorial(String idHistorial) {
+        this.idHistorial = idHistorial;
+    }
+
+    public String getNotasAdicionales() {
+        return notasAdicionales;
+    }
+
+    public void setNotasAdicionales(String notasAdicionales) {
+        this.notasAdicionales = notasAdicionales;
+    }
+
+    public void agregarDiagnostico (){
+
+    }
+
+    public void agregarEnfermedad(){
+
+    }
+
+    public void agregarAlergia(){
+
+    }
+
+    public void recetarMedicamento(){
+
+    }
+
+    public void registro(){
+
+    }
+    
+}
